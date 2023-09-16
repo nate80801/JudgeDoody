9a4e6a0452adef322c10bdf3eef99ec4310dd8f1
Darren Bansil <106610067+libioco@users.noreply.github.com>
2023-09-16 15:08:39 -0400

    Add type animation to home

--- site/client/src/pages/Home.jsx
+++ site/client/src/pages/Home.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import '../styles.scss';
 import judge from '../assets/judge.jpg';
 import graphic from '../assets/graphic.jpg';
+import {TypeAnimation} from 'react-type-animation';
 
 function Home() {
     return(
         <div className='home'>
             <div className='main'>
                 <div className='hero'>
-                    <h1>A striking voice of reason when you need it.</h1>
+                    <TypeAnimation
+                        sequence={[
+                            'A striking voice of reason when you need it.'
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        style={{ fontSize: 50, fontWeight: 300, textAlign: 'center'}}
+                        repeat={1}
+                        />
                     <button type='button'><h2>Invite JudgeDoody</h2></button>
                 </div>
                 <div className='logo'>
