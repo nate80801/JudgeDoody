07241777eea743e2f609d72671f9661ecd1dd1a0
Darren Bansil <106610067+libioco@users.noreply.github.com>
2023-09-16 09:32:27 -0400

    Redo navbar and add styling

--- site/client/src/components/Navbar.jsx
+++ site/client/src/components/Navbar.jsx
@@ -7,12 +7,12 @@ function Navbar() {
         <div className='navbar'>
             <div className='container'>
                 <div className='main'>
-                    <Link to='/'><h1>JudgeDoody</h1></Link>
+                    <Link to='/'><h1>judgedoody</h1></Link>
                 </div>
                 <div className='links'>
-                    <Link class='link' to='/docs'><h4>Documentation</h4></Link>
-                    <Link class='link' to='/about'><h4>About</h4></Link>
-                    <Link>Invite Link</Link>
+                    <Link class='link' to='/docs'><h4>documentation</h4></Link>
+                    <Link class='link' to='/about'><h4>about</h4></Link>
+                    <button><a href="">invite</a></button>
                 </div>
             </div>
         </div>
--- site/client/src/pages/Home.jsx
+++ site/client/src/pages/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import '../styles.scss';
+import judge from '../assets/judge.jpg';
 
 function Home() {
     return(
         <div className='home'>
-            <div className='hero'>
-                <h1>A super cool description of our bot will go here</h1>
-                <button><h2>Invite</h2></button>
+            <div className='main'>
+                <div className='hero'>
+                    <h1>a striking voice of reason when you need it.</h1>
+                    <button type='button'><h2>add to server</h2></button>
+                </div>
+                <div className='logo'>
+                    random image
+                </div>
+            </div>
+            <div className='body'>
+                <h1>a bot that serves justice</h1>
             </div>
         </div>
     );
