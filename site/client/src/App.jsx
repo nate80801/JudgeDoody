9666290f5d7e49844654e7ef159fec5feb3bb7b4
Darren Bansil <106610067+libioco@users.noreply.github.com>
2023-09-16 01:03:21 -0400

    Strip more from vanilla

--- site/client/src/App.jsx
+++ site/client/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-        <div>
-            hello darren
-        </div>
+        <Navbar/>
     </>
   )
 }
