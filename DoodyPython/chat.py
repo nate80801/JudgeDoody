fea8971f220bfdeba94b7093db88aec31533b6ce
Nathan <ndo80801@gmail.com>
2023-09-16 15:08:48 -0400

    Functional program, @'s defendant but not plaintiff

--- DoodyPython/chat.py
+++ DoodyPython/chat.py
@@ -2,14 +2,14 @@ import openai
 openai.api_key = "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"
 
 # Make prompt is to be used in opening_statement
-def make_prompt(plaintiff, defendant, crime) -> str:
+def make_prompt(defendant, plaintiff, crime) -> str:
   # Make the prompt based on the parameters
   return f"""
   Today's trial:
   The defendant {defendant} is accused by plaintiff {plaintiff} of {crime}\n
   You are to introduce yourself as Judge Doody and give an introduction to the court.
-  Do not give a final verdict, only introduce us.
-  End your statement commencing the beginning of the trial
+  Do not give a final verdict, only introduce us in one paragraph.
+  End your statement commencing the beginning of the trial, allowing the plaintiff to start
   """
 
 
@@ -28,10 +28,10 @@ def opening_statement(defendant, plaintiff, crime):
 
 # Pass in the full log of evidence statements (EXPENSIVE FOR API!!)
 # Return [verdict, sentence]
-async def final_judgement(evidence, crime):
+async def final_judgement(evidence, crime, ):
   judge = f"""
-    Review the statements and decide whether the defendant is guilty.
-    You must declare whether they are "Guilty" or "Not Guilty" of {crime} then give closing remarks as to why.
+    Treat the following crime as a serious crime no matter how silly it is. It is very possible for the defendant to be guilty.
+    You must declare whether they are "Guilty" or "Not Guilty" of {crime} (use quotation marks around your final verdict), then give closing remarks as to why.
     Take the jury's decision into account, you may go against the jury's judgement.
   """
 
@@ -66,7 +66,7 @@ async def sentence(verdict, crime):
     model="gpt-3.5-turbo",
     messages=[
       {"role": "system", "content": context},
-      {"role": "user", "content": "You must decide on a funny and wacky sentence for him. Only say the sentence"},
+      {"role": "user", "content": "You must decide a wacky sentence for them. Only say the punishment"},
 
     ]
   )
